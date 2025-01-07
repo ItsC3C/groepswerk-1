d17b7b28d1edd7b051b55578128985a97eda9649
Cédric Van Hoorebeke <cedricvanhoorebeke@outlook.com>
2025-01-07 14:46:30 +0100

    CSS DETAILS 1.1

--- detail.js
+++ detail.js
@@ -1,3 +1,9 @@
-document.querySelector(".pokémon_ability_results").onclick = function () {
-  console.log("Hallo");
-};
+var x = document.querySelectorAll(".pokémon_ability_results");
+var i;
+
+for (i = 0; i < x.length; i++) {
+  x[i].computedStyleMap.display = "none";
+  x[i].addEventListener("mouseover", function () {
+    this.style.display = "block";
+  });
+}
